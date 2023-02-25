6fc6060a3d8eeac27d7012b7523876298495de4d
Ryan Luckie <rtluckie@gmail.com>
2023-02-24 20:37:29 -0600

    Cleanup flake.nix
    - remove unused imports/overlays
    - refresh flake.lock

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
     nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     darwin.url = github:LnL7/nix-darwin;
     home-manager.url = github:nix-community/home-manager;
-    devenv.url = github:cachix/devenv/v0.2;
-    devshell.url = github:numtide/devshell;
+    # devenv.url = github:cachix/devenv/v0.2;
+    # devshell.url = github:numtide/devshell;
     emacs-overlay.url = github:nix-community/emacs-overlay;
-    flake-utils.url = github:numtide/flake-utils;
-    nur.url = github:nix-community/nur;
+    # flake-utils.url = github:numtide/flake-utils;
+    # nur.url = github:nix-community/nur;
     rust-overlay.url = github:oxalica/rust-overlay;
     # follows
     darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -21,19 +21,19 @@
     nixpkgs,
     darwin,
     home-manager,
-    devenv,
-    devshell,
-    nur,
-    flake-utils,
+    # devenv,
+    # devshell,
+    # nur,
+    # flake-utils,
     emacs-overlay,
     rust-overlay,
     ...
   }: let
-    inherit (flake-utils.lib) eachDefaultSystem eachSystem;
+    # inherit (flake-utils.lib) eachDefaultSystem eachSystem;
     inherit (darwin.lib) darwinSystem;
     inherit (inputs.nixpkgs.lib) attrValues makeOverridable optionalAttrs singleton;
     overlays = [
-      nur.overlay
+      # nur.overlay
       emacs-overlay.overlay
       rust-overlay.overlays.default
       (final: prev: {devenv = inputs.devenv.defaultPackage.${prev.system};})
