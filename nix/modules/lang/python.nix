43b1eebe497df385dd8aa85a17011a464df37bb5
Ryan Luckie <rtluckie@gmail.com>
2023-03-02 21:55:13 -0600

    Refactor python modules
    - add install script for pyenv
    - add thefuck dep

--- nix/modules/lang/python.nix
+++ nix/modules/lang/python.nix
@@ -1,7 +1,9 @@
 {
-  config,
   lib,
   pkgs,
+  options,
+  config,
+  inputs,
   ...
 }:
 with lib; let
@@ -18,6 +20,21 @@ in {
   };
 
   config = mkIf cfg.enable (mkMerge [
+    {
+      my.hm.user.home = rec {
+        sessionPath = [
+          "$PYENV_ROOT/bin"
+        ];
+        sessionVariables = {
+          PYENV_ROOT = "$HOME/.local/share/pyenv";
+        };
+      };
+      my.hm.user.programs.zsh.profileExtra = ''
+        export PYENV_ROOT="$HOME/.local/share/pyenv"
+        export PATH="$PYENV_ROOT/bin:$PATH"
+        eval "$(pyenv init --path)"
+      '';
+    }
     {
       my.user.packages = with pkgs; [
         (python3.withPackages (ps:
@@ -38,7 +55,22 @@ in {
           ]))
         poetry
         pipenv
+        thefuck
       ];
+    }
+    {
+      my.hm.user.home.activation.pyenv = with pkgs;
+        inputs.home-manager.lib.hm.dag.entryAfter ["writeBoundary"] ''
+          export PATH=${pkgs.lib.makeBinPath [curl git]}:$PATH
+          export PYENV_ROOT="$HOME/.local/share/pyenv"
+          echo $PYENV_ROOT
+          if [[ ! -d $PYENV_ROOT ]]; then
+            echo "Pyenv not installed. Install now..."
+            curl --silent https://pyenv.run | bash
+          else
+            echo "Pyenv already installed"
+          fi
+        '';
     }
     {
       my.hm.user.programs.zsh.oh-my-zsh.plugins = [
@@ -48,6 +80,7 @@ in {
         "pyenv"
         "pylint"
         "python"
+        "pyenv"
       ];
     }
   ]);
