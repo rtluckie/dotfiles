b415a6e90bf0bca13aedd0e98c9b8f3f0a4f605a
Ryan Luckie <rtluckie@gmail.com>
2023-03-18 04:18:50 -0500

    update nix caches

--- nix/modules/systems/common/nix.nix
+++ nix/modules/systems/common/nix.nix
@@ -20,14 +20,15 @@
       experimental-features = ["nix-command" "flakes"];
       substituters = [
         "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
         "https://cachix.org/api/v1/cache/devenv"
-        "https://cache.nixos.org/"
+        "https://cachix.org/api/v1/cache/emacs"
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "emacs.cachix.org-1:b1SMJNLY/mZF6GxQE+eDBeps7WnkT0Po55TAyzwOxTY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
     gc = {
